099bfb7404a21f09c12c9235d38d24a29c9ca59e
pokergames112 <joao.lsf98@gmail.com>
2022-02-15 00:31:11 -0300

    commit script

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+
+while (c <= 10){
+    console.log(`número ${c}`)
+        c++
+}
+
+
+
+/*var c = 1
+
+do{
+    console.log(`número ${c}`)
+        c++
+}while(c <= 10)*/
+
+
+
+/*console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")*/
